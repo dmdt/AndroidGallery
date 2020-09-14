fb5a6155abcd79497a62092202a7dbb15aa6cf6a
Dmitriy <mr.priskoka@yandex.com>
2020-09-14 20:01:28 +0300

    Add PicturesRepository to download images.

--- app/src/main/java/pw/prsk/gallery/ui/home/NewsAdapter.kt
+++ app/src/main/java/pw/prsk/gallery/ui/home/NewsAdapter.kt
@@ -6,9 +6,15 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.*
 import pw.prsk.gallery.R
+import pw.prsk.gallery.data.PicturesRepository
+import kotlin.coroutines.coroutineContext
 
 class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+    val adapterScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+    val rep = PicturesRepository()
+
     class NewsViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val tvAuthor: TextView = view.findViewById(R.id.tvNewsAuthor)
         val tvDate: TextView = view.findViewById(R.id.tvNewsDate)
@@ -24,6 +30,9 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
         holder.tvAuthor.text = "ASDasd"
         holder.tvDate.text = "asdasdasd"
         holder.tvTitle.text = "$position Title title title title title title"
+        adapterScope.launch {
+            holder.ivImage.setImageBitmap(rep.getPicture("https://picsum.photos/640/480"))
+        }
     }
 
     override fun getItemCount(): Int = 10
