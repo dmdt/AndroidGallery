680bb90fe28fa10bdb703be663e1099d8d6eef7b
Dmitriy <mr.priskoka@yandex.com>
2020-09-23 22:24:17 +0300

    Made loadImages common for APIs below 29.

--- app/src/main/java/pw/prsk/gallery/data/PhotosProvider.kt
+++ app/src/main/java/pw/prsk/gallery/data/PhotosProvider.kt
@@ -12,49 +12,41 @@ import android.util.Size
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+private const val DEBUG_TAG = "PhotosProvider"
+
 class PhotosProvider(var contentResolver: ContentResolver?) {
     suspend fun loadPhotos(): MutableList<Photo> {
         val list: MutableList<Photo> = mutableListOf()
         withContext(Dispatchers.IO) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                val projection = arrayOf(
-                    MediaStore.Images.Media._ID,
-                    MediaStore.Images.Media.DATE_TAKEN,
-                    MediaStore.Images.Media.ORIENTATION,
-                    MediaStore.Images.Media.SIZE
-                )
+            val projection = arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.SIZE
+            )
 
-                val query = contentResolver?.query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    projection,
-                    null,
-                    null,
-                    MediaStore.Images.Media.DATE_TAKEN + " DESC"
-                )
-                query?.use { cursor ->
-                    if (cursor.count != 0) {
-                        Log.i("ASD", "${cursor.count}")
-                        val idIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                        val sizeIndex = cursor.getColumnIndex(MediaStore.Images.Media.SIZE)
-                        while (cursor.moveToNext()) {
-                            val id = cursor.getLong(idIndex)
-                            val size = cursor.getInt(sizeIndex)
+            val query = contentResolver?.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                null,
+                null,
+                MediaStore.Images.Media.DATE_ADDED + " DESC"
+            )
+            query?.use { cursor ->
+                if (cursor.count != 0) {
+                    Log.i(DEBUG_TAG, "Photos found: ${cursor.count}")
+                    val idIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val sizeIndex = cursor.getColumnIndex(MediaStore.Images.Media.SIZE)
+                    while (cursor.moveToNext()) {
+                        val id = cursor.getLong(idIndex)
+                        val size = cursor.getInt(sizeIndex)
 
-                            val contentUri: Uri = ContentUris.withAppendedId(
-                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                                id
-                            )
-                            val photo = Photo(contentUri, size)
-                            list.add(photo)
-                        }
+                        val contentUri: Uri = ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            id
+                        )
+                        val photo = Photo(contentUri, size)
+                        list.add(photo)
                     }
                 }
-            } else {
-                val projection = arrayOf(
-                    MediaStore.Images.Media._ID,
-                    MediaStore.Images.Media.RELATIVE_PATH
-                )
-                //FIXME: Make photos get method for versions below Q
             }
         }
         return list
